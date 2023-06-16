9856ff44e14e4f141fd82f1f29797a919583cb68
Jordan LECLERCQ <jordan.leclercq@toulouse.miage.fr>
2023-06-16 18:33:40 +0200

    annuaire-service integration terminated

--- UsersCours/src/main/java/fr/miage/UsersCours/UsersCoursApplication.java
+++ UsersCours/src/main/java/fr/miage/UsersCours/UsersCoursApplication.java
@@ -2,16 +2,17 @@ package fr.miage.UsersCours;
 
 import fr.miage.UsersCours.repository.UserWithClassRepository;
 import fr.miage.UsersCours.repository.UserWithClassRepositoryImpl;
-import fr.miage.UsersCours.transientobj.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsersCoursApplication {
 
-	public static final String USER_SERVICE_URL = "http://user-service:10000";
+	public static final String USER_SERVICE_URL = "http://USERS-SERVICE";
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersCoursApplication.class, args);
